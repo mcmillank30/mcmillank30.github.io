fae5afa8d68fd1ef5bc3df71b28331600658a7dd
Ubuntu <ubuntu@ip-172-31-93-10.ec2.internal>
2020-04-17 21:02:49 +0000

    EQJS update

--- projects/eloquent-javascript/chapter-04/exercises.js
+++ projects/eloquent-javascript/chapter-04/exercises.js
@@ -27,7 +27,7 @@ var numbers = [];
                 numbers.push(j)
             
           }
-          
+ //otherwise if increment === undefined, count start to end, push k to numbers         
   }else if (increment === undefined){
     
     for(var k = start; k <= end; k++){
@@ -38,6 +38,7 @@ var numbers = [];
 
 
   } 
+//return numbers  
 return numbers
 }
 
@@ -45,18 +46,33 @@ return numbers
 ////////////////////////////////////////////////////////////////////////////////
 // sum /////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
+//Input: array number
+//Output: sum of numbers
+function sum(array) {
+var sum = 0
 
-function sum() {
-
-
-
+//looping array  
+  for(var i = 0; i< array.length; i++){
+//adding array[i] assigning new value to sume    
+    sum += array[i]
+  }
+//returning sum
+return sum
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // reverseArray ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
+//
+function reverseArray(array) {
+var arr = []
 
-function reverseArray() {
+//using for loop to iterate array backwards, and push to empty array
+    for(var i = array.length - 1; i>= 0; i--){
+        arr.push(array[i])
+    }
+//returning arr
+return arr
 
 }
 
@@ -64,7 +80,10 @@ function reverseArray() {
 // reverseArrayInPlace /////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function reverseArrayInPlace() {
+function reverseArrayInPlace(array) {
+//using .reverse to return array in rev
+return array.reverse();
+
 
 }
 
@@ -72,15 +91,34 @@ function reverseArrayInPlace() {
 // arrayToList /////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function arrayToList() {
+function arrayToList(array) {
 
+var list = null;
+//looping decrementing by 1
+  for(var i = array.length - 1; i >= 0; i--){
+ // assing the value of {value: array[i], rest: list} to list  
+    list = {value: array[i], rest: list}
+    
+    }
+//returning list
+return list
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // listToArray /////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function listToArray() {
+function listToArray(list) {
+
+var arr = []
+//loop sublist and pushing node.value to array
+  for (var node = list; node;  node = node.rest){
+    
+    arr.push(node.value)
+    
+  }
+// returning arr
+return arr
 
 }
 
@@ -88,24 +126,72 @@ function listToArray() {
 // prepend /////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function prepend() {
-
+function prepend(value, list) {
+//using listToArray function passing list, assigning to arrList variable
+var arrList = listToArray(list)
+// adding value to begining of arrList
+  arrList.unshift(value)
+ // using arrayToList passing arrList, and assigning to ListObj 
+  var listObj = arrayToList(arrList)
+//return listObj  
+  return listObj
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // nth /////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function nth() {
-
+function nth(list, n) {
+//if not list resolves to true, returning undefined
+if(!list){
+  
+  return undefined
+ //otherwise n == 0 returning list.value 
+} else if (n == 0) {
+  
+  return list.value
+//otherwise returning nth(list.rest, n -1)  
+}else{
+  
+  return nth(list.rest, n - 1)
+}
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // deepEqual ///////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function deepEqual() {
+function deepEqual(a, b) {
+// if a === b, return true
+if(a === b){
+  
+  return true
+//otherwise if a === null || typeof a !== 'object' || b === null || typeof b !== 'object' ; returning false  
+}else if (a === null || typeof a !== 'object' || b === null || typeof b !== 'object'){
+  
+  return false;
+  
+}
+// assigning object keys to variable
+var keysA = Object.keys(a)
+var keysB = Object.keys(b)
 
+// returning false if keysA.length != keysB.length
+if(keysA.length != keysB.length){
+  
+  return false
+  
+}
+//using loop to loop object
+  for(var key of keysA){
+//Returning false if !keysB.includes(key) || !deepEqual(a[key], b[key]))   
+    if(!keysB.includes(key) || !deepEqual(a[key], b[key])){
+      
+        return false
+    }
+  }
+//otherwise returning tre  
+  return true
 }
 
 ////////////////////////////////////////////////////////////////////////////////
--- projects/eloquent-javascript/chapter-05/exercises.js
+++ projects/eloquent-javascript/chapter-05/exercises.js
@@ -2,15 +2,30 @@
 // flatten /////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function flatten() {
-
+function flatten(array) {
+var arr
+//using reduce to loop     
+    array.reduce(function(flat, current){
+ //using concat to add array      
+      arr = flat.concat(current)
+//returning arr      
+      return arr
+    })
+    
+    //returning arr
+return arr
 }
 
 // /////////////////////////////////////////////////////////////////////////////
 // loop ////////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function loop() {
+function loop(start, test, update, body) {
+//using loop and passing array to function
+        for(var value = start; test(value); value = update(value)){
+          
+          body(value)
+        }
 
 }
 
@@ -18,17 +33,48 @@ function loop() {
 // every ///////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function every() {
-
+function every(array, predFunc) {
+//using for in loop
+  for( var element of array){
+ // if !predFunc(element resolve true, returing false
+      if(!predFunc(element)){
+      
+      return false
+      }
+  }
+// returning true  
+return true
 }
 
 // /////////////////////////////////////////////////////////////////////////////
 // dominantDirection ///////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function dominantDirection() {
-
+function dominantDirection(text) {
+// assign count variable 
+var count = countBy(text, char => {
+// assigning script variable. Find script for each char    
+    var script = characterScript(char.codePointAt(0));
+    
+          return script ? script.direction : "none";
+  
+    
+  }).filter(({name}) => name != "none");
+ 
+//if count length === 0 returning "ltr"
+        if (count.length == 0){
+  
+            return "ltr";
 }
+//returning count .reduce passing a,b to fuction
+//Ternary: If a.count > b.count returing a : otherwise b   
+return count.reduce((a, b) => a.count > b.count ? a : b).name;
+}
+
+
+
+
+
 
 // /////////////////////////////////////////////////////////////////////////////
 //  //////////////////////////////////////////////////////
